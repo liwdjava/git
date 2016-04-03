c6842810760796fc12b7b7347db6cd197dbfb7ce
weidong.li <weidong.li@boldseas.com>
2016-04-03 17:50:23 +0800

    update 2

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,6 +8,11 @@ public class Test {
         System.out.print("123");
 
         String s = "123";
+        String s1 = "123";
+        String s2 = "123";
+        String s3 = "123";
+        String s4 = "123";
+
 
     }
 
